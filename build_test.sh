feda7e17ae0da7ffba4c8c62765de97166ddfe18
Jon Jagger <jon@jaggersoft.com>
2020-07-07 19:01:56 +0100

    Provide default to prevent unbound variable error

--- build_test.sh
+++ build_test.sh
@@ -22,7 +22,7 @@ build_image()
 # - - - - - - - - - - - - - - - - - - - - - -
 ip_address()
 {
-  if [ -n "${DOCKER_MACHINE_NAME}" ]; then
+  if [ -n "${DOCKER_MACHINE_NAME:-}" ]; then
     docker-machine ip ${DOCKER_MACHINE_NAME}
   else
     printf localhost
